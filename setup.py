949749562fa8fc12d217c3231965cfd4e5c81d94
nicolas-thomazini <nicolasfrezarimt@gmail.com>
2024-12-15 21:09:15 -0300

    feat: add more options for setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,50 @@
-#libraries
-import pyautogui as gui
-import time
+# Libraries
 import os
 import subprocess
-
-#parameters
-
-#function main, that we choice the option
+from utils.installer import Installer
 
 def main():
-    option = int(input('''Select the option that you want to do: 
-                   [1] Download and Installation Python
-                   [2] Download and Install PIP     
-                   [3] Download and Installation Jupyter
-                   [4] Download and Installation VsCode
-                   [5] Install Git
-                   [6] Install ML Python Packages
-                   [7] Install Docker
-                   '''))
-    
-    match option:
-        case 1:
-            from toolkit_project import Installer
-            Installer.install_python()
-        case _:
-            print("Invalid Option")
-            exit()
+    while True:
+        option = int(input('''Select the option that you want to do: 
+                    [1] Update Sudo       
+                    [2] Download and Installation Python
+                    [3] Download and Install PIP
+                    [4] Upgrade PIP
+                    [5] Download and Install Virtual Enviroment (venv)    
+                    [6] Download and Installation Jupyter Notebook and Lab
+                    [7] Install ML Python Packages
+                    [8] Install Git
+                    [9] Download and Installation VsCode
+                    [10] Install Docker
+                    [0] Exit    
+                    '''))
+
+        match option:
+            case 1:
+                Installer.update()
+            case 2:
+                Installer._python()
+            case 3:
+                Installer._pip()
+            case 4:
+                Installer.up_pip()
+            case 5:
+                Installer._venv()
+            case 6:
+                Installer.jupyter()
+            case 7:
+                Installer.packages()
+            case 8:
+                Installer.git()
+            case 9:
+                Installer.vscode()
+            case 10:
+                Installer.docker()
+            case 0:
+                print("Exiting ML-Toolkit...")
+                break
+            case _:
+                print("Invalid option. Please try again.")
 
 if __name__ == "__main__":
     main()
